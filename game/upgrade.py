840396137b0cdc4deb1319a0eeed928b0bb7382a
Qsgz2023ji5ban <qsgz2023ji5ban@edicdn.eu.org>
2024-06-26 12:11:47 +0800

    666

---
+++ game/upgrade.py
@@ -0,0 +1,101 @@
+import random
+import os
+
+# Randval函数生成随机HP，攻击，暴击和法术值
+def randval(type):
+    if type == 0:
+        randvalue = random.randint(150, 300)
+    elif type == 1:
+        randvalue = random.randint(20, 30)
+    elif type == 2:
+        randvalue = random.randint(40, 50)
+    elif type == 3:
+        randvalue = random.randint(10, 30)
+    else:
+        print("error")
+    return randvalue
+
+# Readfile用于读取settings.txt文件的内容并以列表的形式返回
+def readfile():
+    file = open("settings.txt", "r")
+    lines = file.readlines()
+    return lines
+
+# Writefile用于将新的属性值写入settings.txt文件
+def writefile(data):
+    # 直接覆盖写入settings.txt文件
+    file = open("settings.txt", "w")
+    # 写入新的属性值，data是一个列表
+    for i in data:
+        file.write(str(i)+"\n")
+    file.close()
+
+# changeval用于改变名称，然后并初始化属性，也可直接初始化属性而不改名
+def changeval(data,type):
+    if type > 0:
+        while type < 3:
+            if type == 1:
+                idnum = [0,5,10,15,20,25]
+                namelist = []
+                for i in idnum:
+                    namelist.append(data[i])
+                flag = False
+                while flag == False:
+                    os.system("cls")
+                    print("以下为人员列表：")
+                    for i in range(6):
+                        print(str(i+1)+"："+str(namelist[i]))
+                    s = input("请输入要修改的名称：")
+                    if str(s) in namelist:
+                        t = input("请输入新的名称：")
+                        if t not in namelist:
+                            # 更改名称
+                            data[idnum[namelist.index(s)]] = t
+                            # 也应用于namelist
+                            namelist[namelist.index(s)] = t
+                    u = input("是否继续修改？(Y/N)")
+                    if u == "Y" or u == "y":
+                        flag = False
+                    elif u == "N" or u == "n":
+                        flag = True
+                    else:
+                        print("输入错误，请重新输入")
+                        flag = False
+                type += 1
+            elif type == 2:
+                hp=[1,6,11,16,21,26]
+                atk=[2,7,12,17,22,27]
+                sup=[3,8,13,18,23,28]
+                magic=[4,9,14,19,24,29]
+                for i in range(6):
+                    data[hp[i]] = randval(0)
+                for i in range(6):
+                    data[atk[i]] = randval(1)
+                for i in range(6):
+                    data[sup[i]] = randval(2)
+                for i in range(6):
+                    # 这里改为0
+                    data[magic[i]] = 0
+                    
+                type += 1
+    else:
+        data = "非法"
+    return data
+
+# alive用于判定并输出存活名单供用户选择
+#def alive(data,group):
+#    if group == 0:
+#        idnum = [0,5,10,15,20,25]
+i = range(30)
+#将i中元素全部转化为字符串
+i = [str(i) for i in i]
+
+i = readfile()
+
+changeval(i,1)
+
+writefile(i)
+
+print(i)
+
+# 读取文件 
